5e3506465d9f655c0adffdfa7b7465c3527eef97
Wellington Barbosa <carneirobarbosawellington@gmail.com>
2020-09-02 06:53:38 -0300

    Fechar alerta

--- resources/views/components/alert.blade.php
+++ resources/views/components/alert.blade.php
@@ -1,5 +1,5 @@
 @if ($errors->any())
-    <div class="alert alert-warning" role="alert">
+    <div class="alert alert-warning alert-dismissible" role="alert">
         <span class="alert-icon"><i class="fas fa-warning"></i></span>
         <span class="alert-text"><strong>Ops...</strong>
             <ul>
@@ -8,20 +8,29 @@
                 @endforeach
             </ul>
         </span>
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
     </div>
 @endif
 
 @if(session('error'))
-<div class="alert alert-danger" role="alert">
+<div class="alert alert-danger alert-dismissible" role="alert">
     <span class="alert-icon"><i class="fas fa-thumbs-down"></i></span>
     <span class="alert-text"><strong>Ops...</strong><span class="ml-2">{{ session('error') }}</span></span>
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+    </button>
 </div>
 @endif
 
 @if(session('success'))
-<div class="alert alert-success" role="alert">
+<div class="alert alert-success alert-dismissible" role="alert">
     <span class="alert-icon"><i class="fas fa-thumbs-up"></i></span>
     <span class="alert-text"><strong>Sucesso!</strong><span class="ml-2">{{ session('success') }}</span></span>
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+    </button>
 </div>
 @endif
 
--- resources/views/components/alloom-customer/nav.blade.php
+++ resources/views/components/alloom-customer/nav.blade.php
@@ -15,7 +15,7 @@
             <div class="collapse navbar-collapse" id="sidenav-collapse-main">
                 <!-- Nav items -->
                 <ul class="navbar-nav">
-                    <li class="nav-item"><a class="nav-link active" href="#navbar-dashboards" data-toggle="collapse" role="button" aria-expanded="true" aria-controls="navbar-dashboards"><i class="ni ni-shop text-primary"></i> <span class="nav-link-text">Produtos</span></a>
+                    <li class="nav-item"><a class="nav-link {{ request()->routeIs('alloom_customer.products*') ? 'active' : '' }}" href="#navbar-dashboards" data-toggle="collapse" role="button" aria-expanded="true" aria-controls="navbar-dashboards"><i class="ni ni-shop text-primary"></i> <span class="nav-link-text">Produtos</span></a>
                         <div class="collapse show" id="navbar-dashboards">
                             <ul class="nav nav-sm flex-column">
                                 <li class="nav-item"><a href="{{ route('alloom_customer.products.create') }}" class="nav-link"><span class="sidenav-mini-icon"></span><span class="sidenav-normal">Novo</span></a></li>
