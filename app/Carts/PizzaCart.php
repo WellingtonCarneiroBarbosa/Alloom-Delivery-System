2ba3ffe5433401e4f770e188b62310783dd431fa
wellington barbosa <carneirobarbosawellington@gmail.com>
2020-09-16 03:19:55 -0300

    Pizza Cart Class

---
+++ app/Carts/PizzaCart.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Carts;
+
+class PizzaCart
+{
+    protected array $pizzas;
+    protected string $totalQuantity;
+    protected string $totalPrice;
+
+    public function __construct($oldCart)
+    {
+        if($oldCart) {
+            $this->pizzas = $oldCart->pizzas;
+            $this->totalQuantity = $oldCart->totalQuantity;
+            $this->totalPrice = $oldCart->totalPrice;
+        }
+    }
+
+    public function add($border=null, $flavors, $size, $quantity) {
+        $pizza = [
+            "border" => $border,
+            "flavors" => $flavors,
+            "size" => $size,
+            "quantity" => $quantity,
+            "unit_price" => $this->getPizzaPrice($flavors, $border),
+            "total_price" => 0,
+        ];
+
+        $piza["total_price"] = $pizza["unit_price"] * $pizza["quantity"];
+
+        $this->totalQuantity += $pizza["quantity"];
+        $this->totalPrice += $pizza["total_price"];
+    }
+
+    protected function getPizzaPrice($flavors, $border) {
+
+        $quantity_flavors = count($flavors);
+
+        $flavor_price_sum = 0;
+
+        foreach($flavors as $flavor) {
+            $flavor_price_sum += $flavor->price;
+        }
+
+        $pizza_price = $flavor_price_sum / $quantity_flavors;
+
+        if($border) {
+            $pizza_price += $border->price;
+        }
+
+        return $pizza_price;
+    }
+}
